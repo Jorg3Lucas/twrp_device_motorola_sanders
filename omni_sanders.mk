6d79e343527c3a1a43c7d1781d6919824d59eab6
durasame <daffetyxd@gmail.com>
2021-03-22 13:28:17 -0300

    sanders: Cleanup sanders makefile
    Signed-off-by: durasame <daffetyxd@gmail.com>
    Change-Id: Ic4fa670fdc10bb2bcbd9fc1230ca0b22336a4219

--- omni_sanders.mk
+++ omni_sanders.mk
@@ -15,17 +15,11 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit from sanders device
-$(call inherit-product, device/motorola/sanders/device.mk)
-
-# Inherit some common Omni stuff.
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
